3ca2c59d962c479300f94657f3d3c3e3cf3ea9a6
Jose Lorenzo <jose@worklytics.co>
2022-09-22 16:22:46 -0700

    Add basic AWS elasticache redis node

--- infra/examples-dev/aws-google-workspace/main.tf
+++ infra/examples-dev/aws-google-workspace/main.tf
@@ -35,11 +35,11 @@ provider "aws" {
 
 locals {
   base_config_path = "${var.psoxy_base_dir}/configs/"
-  bulk_sources     = {
+  bulk_sources = {
     "hris" = {
       source_kind = "hris"
-      rules       = {
-        columnsToRedact       = []
+      rules = {
+        columnsToRedact = []
         columnsToPseudonymize = [
           "email",
           "employee_id"
@@ -48,8 +48,8 @@ locals {
     },
     "qualtrics" = {
       source_kind = "qualtrics"
-      rules       = {
-        columnsToRedact       = []
+      rules = {
+        columnsToRedact = []
         columnsToPseudonymize = [
           "email",
           "employee_id"
@@ -176,10 +176,25 @@ module "worklytics-psoxy-connection-google-workspace" {
 
 # BEGIN AUTH CONNECTORS
 
+# Redis backend to share access tokens across multiple lambda instances
+resource "aws_elasticache_cluster" "redis-backend" {
+  count                = length(module.worklytics_connector_specs.enabled_oauth_long_access_connectors) > 0 ? 1 : 0
+  cluster_id           = "psoxy-redis-backend"
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis6.x"
+  engine_version       = "6.2"
+  port                 = 6379
+  connection {
+    password = "whatever"
+  }
+}
+
 # Create secure parameters (later filled by customer)
 # Can be later passed on to a module and store in other vault if needed
 resource "aws_ssm_parameter" "long-access-secrets" {
-  for_each = {for entry in module.worklytics_connector_specs.enabled_oauth_secrets_to_create : "${entry.connector_name}.${entry.secret_name}" => entry}
+  for_each = { for entry in module.worklytics_connector_specs.enabled_oauth_secrets_to_create : "${entry.connector_name}.${entry.secret_name}" => entry }
 
   name        = "PSOXY_${upper(replace(each.value.connector_name, "-", "_"))}_${upper(each.value.secret_name)}"
   type        = "SecureString"
@@ -223,7 +238,7 @@ module "aws-psoxy-long-auth-connectors" {
   path_to_repo_root              = var.psoxy_base_dir
   example_api_calls              = each.value.example_api_calls
   reserved_concurrent_executions = each.value.reserved_concurrent_executions
-  parameters                     = [
+  parameters = [
     module.psoxy-aws.salt_secret,
     # aws_ssm_parameter.long-access-secrets[each.key]
   ]
